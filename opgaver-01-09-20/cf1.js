7bfd7a9d48d035832d43ee07bca3196a76c9d54c
Charlotte <c.libak@hotmail.com>
2020-09-01 12:06:57 +0200

    added html and js file

---
+++ opgaver-01-09-20/cf1.js
@@ -0,0 +1,79 @@
+'use strict';
+//no changes has been made 
+
+let tails = 0;
+let heads = 0;
+let flip;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+flip = Math.random();
+flip = Math.floor(flip * 2 + 1);
+if (flip === 1)
+    tails++;
+else
+    heads++;
+
+console.log('heads: ' + heads);
+console.log('tails: ' + tails);
