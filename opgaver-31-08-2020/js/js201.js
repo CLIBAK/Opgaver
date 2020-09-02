202c44699fdba98c5c63c71fe85d1e439263d0ed
Charlotte <c.libak@hotmail.com>
2020-09-02 08:26:01 +0200

    js filer fra 31-08-20

--- opgaver-31-08-2020/js/js201.js
+++ opgaver-31-08-2020/js/js201.js
@@ -1,22 +1,5 @@
 let year = prompt("Enter year");
 
-function leapyear(year) {
-    return year % 100 === 0 ? year % 400 === 0 : year % 4 === 0;
-  }
-  console.log(leapyear(year));
-
-
-  let cpr = prompt("Enter cpr-nummer");
-
-  function woman(cpr){
-    return cpr % 2 === 0;
-  }
-  console.log("This is a woman : " + woman(cpr));
-
-  let x = 8 / 2 * (2 + 2);
-
-  console.log(x);
-
-  let y = 8 / 2 * 2 + 2;
-
-  console.log(y);
+let leapyear = ( year % 100 !== 0 ) && (year % 400 == 0) || (year % 4 == 0);
+  
+  console.log(leapyear);
