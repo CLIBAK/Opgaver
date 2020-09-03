4877abc23bc317c231b9ac7c61f23840d2ca4935
Charlotte <c.libak@hotmail.com>
2020-09-03 22:53:07 +0200

    opgaver fra 03-09-2020 og rettelser i js file fra 31-08-2020

--- opgaver-31-08-2020/js/js201.js
+++ opgaver-31-08-2020/js/js201.js
@@ -1,5 +1,5 @@
 let year = prompt("Enter year");
 
-let leapyear = ( year % 100 !== 0 ) && (year % 400 == 0) || (year % 4 == 0);
+let leapyear = ( year % 4 !== 0  && !(year % 100 == 0)) || year % 400 == 0;
   
   console.log(leapyear);
